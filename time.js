5f19709a94fb65684c8cc64b8c635e4ec5ffb186
Victor Li <victor.li158@gmail.com>
2017-04-27 13:40:01 -0700

    Initial commit

---
+++ time.js
@@ -0,0 +1,17 @@
+class Clock {
+  constructor() {
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    printTime();
+  }
+
+  printTime() {
+    
+  }
+
+  _time() {
+
+  }
+}
